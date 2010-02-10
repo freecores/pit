c5f501b74b4da7c26132cbafff7c1d0eaa752c08
rehayes <rehayes@3ee77c1c-26a4-411c-8126-4402bdb4877f>
2010-02-10 16:35:47 +0000

    Traded 16 data registers for 3 address registers when wait states are enabled.

--- rtl/verilog/pit_wb_bus.v
+++ rtl/verilog/pit_wb_bus.v
@@ -41,7 +41,7 @@ module pit_wb_bus #(parameter ARST_LVL = 1'b0,    // asynchronous reset level
                     parameter SINGLE_CYCLE = 1'b0)
   (
   // Wishbone Signals
-  output      [DWIDTH-1:0] wb_dat_o,     // databus output
+  output reg  [DWIDTH-1:0] wb_dat_o,     // databus output - Pseudo Register
   output                   wb_ack_o,     // bus cycle acknowledge output
   input                    wb_clk_i,     // master clock input
   input                    wb_rst_i,     // synchronous active high reset
@@ -62,15 +62,15 @@ module pit_wb_bus #(parameter ARST_LVL = 1'b0,    // asynchronous reset level
 
 
   // registers
-  reg                bus_wait_state;  // Holdoff wb_ack_o for one clock to add wait state
-  reg  [DWIDTH-1:0]  rd_data_mux;     // Pseudo Register, WISHBONE Read Data Mux
-  reg  [DWIDTH-1:0]  rd_data_reg;     // Latch for WISHBONE Read Data
+  reg        bus_wait_state;  // Holdoff wb_ack_o for one clock to add wait state
+  reg  [2:0] addr_latch;      // Capture WISHBONE Address 
 
   // Wires
-  wire   eight_bit_bus;
-  wire   module_sel;      // This module is selected for bus transaction
-  wire   wb_wacc;         // WISHBONE Write Strobe
-  wire   wb_racc;         // WISHBONE Read Access (Clock gating signal)
+  wire       eight_bit_bus;
+  wire       module_sel;      // This module is selected for bus transaction
+  wire       wb_wacc;         // WISHBONE Write Strobe
+  wire       wb_racc;         // WISHBONE Read Access (Clock gating signal)
+  wire [2:0] address;         // Select either direct or latched address
 
   //
   // module body
@@ -87,7 +87,7 @@ module pit_wb_bus #(parameter ARST_LVL = 1'b0,    // asynchronous reset level
   assign wb_wacc    = module_sel && wb_we_i && (wb_ack_o || SINGLE_CYCLE);
   assign wb_racc    = module_sel && !wb_we_i;
   assign wb_ack_o   = SINGLE_CYCLE ? module_sel : (bus_wait_state && module_sel);
-  assign wb_dat_o   = SINGLE_CYCLE ? rd_data_mux : rd_data_reg;
+  assign address    = SINGLE_CYCLE ? wb_adr_i : addr_latch;
 
   // generate acknowledge output signal, By using register all accesses takes two cycles.
   //  Accesses in back to back clock cycles are not possable.
@@ -99,25 +99,26 @@ module pit_wb_bus #(parameter ARST_LVL = 1'b0,    // asynchronous reset level
     else
       bus_wait_state <=  module_sel && !bus_wait_state;
 
-  // assign data read bus -- DAT_O
+  // Capture address in first cycle of WISHBONE Bus tranaction
+  //  Only used when Wait states are enabled
   always @(posedge wb_clk_i)
-    if ( wb_racc )                     // Clock gate for power saving
-      rd_data_reg <= rd_data_mux;
+    if ( module_sel )                  // Clock gate for power saving
+      addr_latch <= wb_adr_i;
 
   // WISHBONE Read Data Mux
   always @*
-      case ({eight_bit_bus, wb_adr_i}) // synopsys parallel_case
+      case ({eight_bit_bus, address}) // synopsys parallel_case
       // 8 bit Bus, 8 bit Granularity
-      4'b1_000: rd_data_mux = read_regs[ 7: 0];  // 8 bit read address 0
-      4'b1_001: rd_data_mux = read_regs[15: 8];  // 8 bit read address 1
-      4'b1_010: rd_data_mux = read_regs[23:16];  // 8 bit read address 2
-      4'b1_011: rd_data_mux = read_regs[31:24];  // 8 bit read address 3
-      4'b1_100: rd_data_mux = read_regs[39:32];  // 8 bit read address 4
-      4'b1_101: rd_data_mux = read_regs[47:40];  // 8 bit read address 5
+      4'b1_000: wb_dat_o = read_regs[ 7: 0];  // 8 bit read address 0
+      4'b1_001: wb_dat_o = read_regs[15: 8];  // 8 bit read address 1
+      4'b1_010: wb_dat_o = read_regs[23:16];  // 8 bit read address 2
+      4'b1_011: wb_dat_o = read_regs[31:24];  // 8 bit read address 3
+      4'b1_100: wb_dat_o = read_regs[39:32];  // 8 bit read address 4
+      4'b1_101: wb_dat_o = read_regs[47:40];  // 8 bit read address 5
       // 16 bit Bus, 16 bit Granularity
-      4'b0_000: rd_data_mux = read_regs[15: 0];  // 16 bit read access address 0
-      4'b0_001: rd_data_mux = read_regs[31:16];
-      4'b0_010: rd_data_mux = read_regs[47:32];
+      4'b0_000: wb_dat_o = read_regs[15: 0];  // 16 bit read access address 0
+      4'b0_001: wb_dat_o = read_regs[31:16];
+      4'b0_010: wb_dat_o = read_regs[47:32];
     endcase
 
   // generate wishbone write register strobes -- one hot if 8 bit bus
@@ -125,7 +126,7 @@ module pit_wb_bus #(parameter ARST_LVL = 1'b0,    // asynchronous reset level
     begin
       write_regs = 0;
       if (wb_wacc)
-	case ({eight_bit_bus, wb_adr_i}) // synopsys parallel_case
+	case ({eight_bit_bus, address}) // synopsys parallel_case
            // 8 bit Bus, 8 bit Granularity
 	   4'b1_000 : write_regs = 4'b0001;
 	   4'b1_001 : write_regs = 4'b0010;
